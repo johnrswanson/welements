611b420597a5ee666b3ce275573b8a4b97090795
John Swanson <john@red.yellow.blue>
2015-11-16 13:19:51 -0500

    Add touchpunch for iPad drag drop /resize

--- index.php
+++ index.php
@@ -17,8 +17,9 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 
 
-<script type="text/javascript" type="text/javascript" src="admin/admin.js"></script>
-<script type="text/javascript" type="text/javascript" src="helper/helper.js"></script>
+<script type="text/javascript" src="admin/admin.js"></script>
+<script type="text/javascript" src="helper/helper.js"></script>
+<script type="text/javascript" src="navigation/touchpunch.js"></script>
 
 
 
